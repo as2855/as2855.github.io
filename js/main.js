2c6c5d16604ddd83a164008777ae735d8e2c1d8e
Alice Song <alicesong@Alices-MacBook-Pro.local>
2020-02-12 23:29:54 -0500

    try other theme

--- js/main.js
+++ js/main.js
@@ -1,128 +1,67 @@
-/* =================================
-------------------------------------
-	Riddle - Portfolio
-	Version: 1.0
- ------------------------------------ 
- ====================================*/
+/*
+* Template Name: MyPortfolio
+* Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
+* License: https://bootstrapmade.com/license/
+*/
+
+(function ($) {
+  "use strict";
+
+  var burgerMenu = function() {
+	  $('.burger').click(function(e) {
+	  	$(window).scrollTop(0);
+	    if(!$('.burger').hasClass('active'))
+	      $('.burger').addClass('active');
+	    else
+	      $('.burger').removeClass('active');
+	  });
+  }
+  burgerMenu();
+
+  var siteIstotope = function() {
+	  var $container = $('#portfolio-grid').isotope({
+	    itemSelector : '.item',
+	    isFitWidth: true
+	  });
+
+	  $(window).resize(function(){
+	    $container.isotope({
+	      columnWidth: '.col-sm-3'
+	    });
+	  });
+	  
+	  $container.isotope({ filter: '*' });
+
+	  $('#filters').on( 'click', 'a', function(e) {
+	  	e.preventDefault();
+	    var filterValue = $(this).attr('data-filter');
+	    $container.isotope({ filter: filterValue });
+	    $('#filters a').removeClass('active');
+	    $(this).addClass('active');
+	  });
+  }
+  $(window).on('load', function () {
+    siteIstotope();
+  });
 
 
+  var siteOwlCarousel = function() {
+  	$('.testimonial-carousel').owlCarousel({
+		  center: true,
+	    items: 1,
+	    loop: true,
+	    margin: 0,
+	    autoplay: true,
+	    smartSpeed: 1000,
+		});
+  };
+  siteOwlCarousel();
 
-'use strict';
-
-
-$(window).on('load', function() { 
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
-
-	if($('.portfolios-area').length > 0 ) {
-		var containerEl = document.querySelector('.portfolios-area');
-		var mixer = mixitup(containerEl);
-	}
-
-});
-
-
-(function($) {
-
-	/*------------------
-		Navigation
-	--------------------*/
-	$('.nav-switch').on('click', function(event) {
-		$('.main-menu').slideToggle(400);
-		event.preventDefault();
-	});
-
-
-
-	/*------------------
-		Background set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
-
-
-
-	/*----------------------
-		Portfolio layout
-	------------------------*/
-	var port_fi =  $('.portfolios-area .first-item'),
-		port_si =  $('.portfolios-area .second-item'),
-		port_intro_h =  $('.portfolio-intro').innerHeight();
-
-	if ($(window).width() > 991) {
-		port_fi.appendTo('.portfolio-intro');
-		port_si.find('.portfolio-item').height(port_intro_h + 601);
-	}
-
-	$('.portfolio-item.pi-style2').each(function() {
-		var pi_width = $(this).width();
-		$(this).height(pi_width + 50);
-	});
-
-
-
-	/*------------------
-		Popup
-	--------------------*/
-	$('.portfolio-item').magnificPopup({
-		type: 'image',
-		mainClass: 'img-popup-warp',
-		removalDelay: 400,
-	});
-
-
-	/*------------------
-		Accordions
-	--------------------*/
-	$('.panel-link').on('click', function (e) {
-		$('.panel-link').parent('.panel-header').removeClass('active');
-		var $this = $(this).parent('.panel-header');
-		if (!$this.hasClass('active')) {
-			$this.addClass('active');
-		}
-		e.preventDefault();
-	});
-
-
-if($().circleProgress){
-
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 0.75,
-		size: 146,
-		thickness: 3,
-		fill: "#979797",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.83,
-		size: 146,
-		thickness: 3,
-		fill: "#979797",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 3
-	$("#progress3").circleProgress({
-		value: 0.25,
-		size: 146,
-		thickness: 3,
-		fill: "#979797",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 4
-	$("#progress4").circleProgress({
-		value: 0.95,
-		size: 146,
-		thickness: 3,
-		fill: "#979797",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-}
 
 })(jQuery);
+
+AOS.init({
+	easing: 'ease',
+	duration: 1000,
+	once: true
+});
